a79ba03043122c2444562e269d3d56f1b8e3149b
Pedr0 <pedrovbpereira20@gmail.com>
2024-08-15 19:53:56 -0300

    Adicionei a função "div" para meu projeto de calculadora

--- 20240009788_pedro/calculadora.sh
+++ 20240009788_pedro/calculadora.sh
@@ -3,12 +3,13 @@ echo Insira o primeiro valor:
 read a
 echo Insira o segundo valor:
 read b
-echo add, sub, mult
+echo add, sub, mult, div
 read c
 
 case $c in
 	add) echo $((a+b)) ;;
 	sub) echo $((a-b)) ;;
 	mult) echo $((a*b)) ;;
+	div) echo $((a/b)) ;;
 esac
 
