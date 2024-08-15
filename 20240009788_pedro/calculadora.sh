23f4a3ec0a33cb4117d90385f08a9906d0c3ad4f
Pedr0 <pedrovbpereira20@gmail.com>
2024-08-15 19:52:45 -0300

    Adicionei a função "mult" para meu projeto de calculadora

--- 20240009788_pedro/calculadora.sh
+++ 20240009788_pedro/calculadora.sh
@@ -3,11 +3,12 @@ echo Insira o primeiro valor:
 read a
 echo Insira o segundo valor:
 read b
-echo add, sub
+echo add, sub, mult
 read c
 
 case $c in
 	add) echo $((a+b)) ;;
 	sub) echo $((a-b)) ;;
+	mult) echo $((a*b)) ;;
 esac
 
